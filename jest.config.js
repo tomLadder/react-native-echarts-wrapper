d6410a139de832bf451c2750b54502fa875ecc23
tomLadder <thomas.leiter@outlook.at>
2019-10-04 15:55:44 +0200

    fixed tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
   notify: true,
   verbose: false,
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
   },
-  testPathIgnorePatterns: ['node_modules/', 'example/'],
-  transformIgnorePatterns: ['node_modules/(?!react-native)', 'example/'],
-  coverageDirectory: './coverage/',
+  testPathIgnorePatterns: ["node_modules/", "<rootDir>/src/__tests__/setup/"],
+  transformIgnorePatterns: ["node_modules/(?!react-native)", "example/"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup/setupJest.js"],
+  coverageDirectory: "./coverage/",
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.js',
-  ],
-};
+  collectCoverageFrom: ["src/**/*.js"]
+}
